4aa74ab7605e0b735b5d53e45491bb71f9b860f4
Darius Bacon <darius@wry.me>
2013-06-23 12:44:39 -0700

    tweaking and optimizing

--- twirl.js
+++ twirl.js
@@ -4,33 +4,33 @@ var width = canvas.width;
 var height = canvas.height;
 var ctx = canvas.getContext("2d");
 
-var ww = (width/10) | 0;
-var hh = (height/10) | 0;
+var radius = 2.5;
+var diameter = 2*radius;
+var minspeed = .001;
+var maxspeed = .003;
+
+var ww = (width/diameter) | 0;
+var hh = (height/diameter) | 0;
 
 function tick(delta, time) {
     ctx.strokeStyle = 'white';
-    ctx.lineWidth = 1;
+    ctx.lineWidth = 1; //0.75 + 0.5 * Math.sin(0.0001 * time);
     ctx.clearRect(0, 0, width, height);
-    for (var yy = 0; yy < hh; ++yy)
-        for (var xx = 0; xx < ww; ++xx) {
-            var cx = 5 + 10*xx;
-            var cy = 5 + 10*yy;
-            var rate = (xx + yy) / (hh + ww);
-            spin(cx, cy, 5, rate, time);
-        }
-}
-
-var minspeed = .001;
-var maxspeed = .010;
-
-function spin(cx, cy, r, rate, time) {
-    var omega = minspeed + (maxspeed-minspeed) * rate;
-    var angle = omega * time;
-    var x = r * Math.cos(angle);
-    var y = r * Math.sin(angle);
     ctx.beginPath();
-    ctx.moveTo(cx - x, cy - y);
-    ctx.lineTo(cx + x, cy + y);
+    for (var sum = 0; sum < hh+ww; ++sum) {
+        var rate = sum / (hh + ww);
+        var omega = minspeed + (maxspeed-minspeed) * rate;
+        var angle = omega * time;
+        var x = (2+radius) * Math.cos(angle);
+        var y = (2+radius) * Math.sin(angle);
+        for (var yy = 0; yy < hh; ++yy) {
+            var xx = sum - yy;
+            var cx = radius + diameter*xx;
+            var cy = radius + diameter*yy;
+            ctx.moveTo(cx - x, cy - y);
+            ctx.lineTo(cx + x, cy + y);
+        }
+    }
     ctx.stroke();
 }
 
