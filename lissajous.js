efa90263a487f4e05398bff66582a99d2d53f0b6
Darius Bacon <darius@wry.me>
2013-06-29 14:53:51 -0700

    swap front/back canvases and rename them; use x and y colors more

--- lissajous.js
+++ lissajous.js
@@ -1,21 +1,21 @@
 'use strict';
 
-var width = backCanvas.width;
-var height = backCanvas.height;
+var width = sizeCanvas.width;
+var height = sizeCanvas.height;
 var scale = Math.min(width, height) / 2;
-var ctx = backCanvas.getContext('2d');
+var ctx = sizeCanvas.getContext('2d');
 
-var frontCtx = frontCanvas.getContext('2d');
+var curveCtx = curveCanvas.getContext('2d');
 
-var backCanvasBounds = backCanvas.getBoundingClientRect();
+var sizeCanvasBounds = sizeCanvas.getBoundingClientRect();
 var startMouseX, startMouseY;
 var startXRadius, startYRadius;
 var mouseX = 100;
 var mouseY = 20;
 var stale = true;
 function onMousemove(event) {
-    var mouseX = event.clientX - backCanvasBounds.left;
-    var mouseY = event.clientY - backCanvasBounds.top;
+    var mouseX = event.clientX - sizeCanvasBounds.left;
+    var mouseY = event.clientY - sizeCanvasBounds.top;
     var dx = mouseX - startMouseX;
     var dy = mouseY - startMouseY;
     xradius = Math.abs(startXRadius + dx * (startMouseX < width/2 ? -1 : 1));
@@ -23,21 +23,24 @@ function onMousemove(event) {
 }
 
 function onMousedown(event) {
-    startMouseX = event.clientX - backCanvasBounds.left;
-    startMouseY = event.clientY - backCanvasBounds.top;
+    startMouseX = event.clientX - sizeCanvasBounds.left;
+    startMouseY = event.clientY - sizeCanvasBounds.top;
     startXRadius = xradius;
     startYRadius = yradius;
-    frontCanvas.addEventListener('mousemove', onMousemove, true);
+    sizeCanvas.addEventListener('mousemove', onMousemove, true);
 }
 function onMouseup(event)   {
-    frontCanvas.removeEventListener('mousemove', onMousemove, true);
+    sizeCanvas.removeEventListener('mousemove', onMousemove, true);
     stale = true;
 }
 
-frontCanvas.addEventListener('mousedown', onMousedown);
-frontCanvas.addEventListener('mouseup', onMouseup);
+sizeCanvas.addEventListener('mousedown', onMousedown);
+sizeCanvas.addEventListener('mouseup', onMouseup);
 
 var tau = 2 * Math.PI;
+
+var xcolor = 'green';           //'rgb(0,255,0)';
+var ycolor = 'magenta';         // 'rgb(255,0,0)';
 
 var xrate = .5;  // in (0..1]
 var yrate = .5;  // in (0..1]
@@ -55,46 +58,46 @@ var time = 0;
 function tick() {
     ctx.clearRect(0, 0, width, height);
     if (stale)
-        frontCtx.clearRect(0, 0, width, height);
-
-    ctx.strokeStyle = 'grey';
-    ctx.beginPath();
-    ctx.arc(width/2, height/2, xradius, 0, tau, true);
-    ctx.stroke();
-    ctx.beginPath();
-    ctx.arc(width/2, height/2, yradius, 0, tau, true);
-    ctx.stroke();
+        curveCtx.clearRect(0, 0, width, height);
 
     var x = Math.cos(xomega * time);
     var y = Math.sin(xomega * time);
     var xx = width/2 + x*xradius;
     var xy = height/2 - y*xradius;
 
-    ctx.fillStyle = 'green';
-    ctx.beginPath();
-    ctx.arc(xx, xy, 4, 0, tau, true);
-    ctx.fill();
-
     x = Math.cos(yomega * time + yphase);
     y = Math.sin(yomega * time + yphase);
     var yx = width/2 + x*yradius;
     var yy = height/2 - y*yradius;
 
-    ctx.fillStyle = 'red';
+    ctx.fillStyle = ctx.strokeStyle = xcolor;
+    ctx.beginPath();
+    ctx.arc(width/2, height/2, xradius, 0, tau, true);
+    ctx.stroke();
+    ctx.beginPath();
+    ctx.moveTo(xx, xy);
+    ctx.lineTo(xx, yy);
+    ctx.stroke();
+    ctx.beginPath();
+    ctx.arc(xx, xy, 4, 0, tau, true);
+    ctx.fill();
+
+    ctx.fillStyle = ctx.strokeStyle = ycolor;
+    ctx.beginPath();
+    ctx.arc(width/2, height/2, yradius, 0, tau, true);
+    ctx.stroke();
+    ctx.beginPath();
+    ctx.moveTo(xx, yy);
+    ctx.lineTo(yx, yy);
+    ctx.stroke();
     ctx.beginPath();
     ctx.arc(yx, yy, 4, 0, tau, true);
     ctx.fill();
 
-    ctx.beginPath();
-    ctx.moveTo(xx, xy);
-    ctx.lineTo(xx, yy);
-    ctx.lineTo(yx, yy);
-    ctx.stroke();
-
-    frontCtx.fillStyle = 'white';
-    frontCtx.beginPath();
-    frontCtx.arc(xx, yy, 1.5, 0, tau, true);
-    frontCtx.fill();
+    curveCtx.fillStyle = 'white';
+    curveCtx.beginPath();
+    curveCtx.arc(xx, yy, 1.5, 0, tau, true);
+    curveCtx.fill();
 
     time += 1;
     stale = false;
