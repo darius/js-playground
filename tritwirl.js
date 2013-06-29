832ffb56936ee443b3f61f47a58ba8e8df96881e
Darius Bacon <darius@wry.me>
2013-06-29 14:52:41 -0700

    shrink if we're too slow; tweaked sizes/rates.

--- tritwirl.js
+++ tritwirl.js
@@ -4,17 +4,44 @@ var width = canvas.width;
 var height = canvas.height;
 var ctx = canvas.getContext('2d');
 
-var radius = 10;
+var radius = 12;
 var diameter = 2*radius;
 var minspeed = .001;
-var maxspeed = .010;
+var maxspeed = .003 // .008;
 var phasing = .00005;
 var breathing = -.000081;
 
 var ww = (width/diameter) | 0;
 var hh = (height/diameter) | 0;
 
+var N = 8;  // delta < 20 ? ww - 2 : ww - 20;             // XXX hand-hacked
+
+var recentDeltas = [0, 0, 0, 0, 0, 0, 0, 0,];
+var rd = 0;
+var ndeltas = 0;
+
+function areWeFastEnough(delta) {
+    recentDeltas[rd] = delta;
+    rd = (rd + 1) % recentDeltas.length;
+    var all = true;
+    var any = false;
+    for (var i = 0; i < recentDeltas.length; ++i) {
+        if (recentDeltas[i] < 67) // We want at least 15 fps
+            any = true;
+        else
+            all = false;
+    }
+    return all ? 1 : any ? 0 : -1;
+}
+
 function tick(delta, time) {
+    if (++ndeltas < 120) {
+        var fastEnough = areWeFastEnough(delta);
+        if (fastEnough === 1 && N < ww - 2)
+            ++N;
+        else if (fastEnough === -1 && 4 < N)
+            --N;
+    }
     ctx.lineWidth = 4;
     ctx.clearRect(0, 0, width, height);
     ctx.globalCompositeOperation = 'lighter';
@@ -28,7 +55,6 @@ function spinspinspin(time) {
     var phase1 = phasing * time;
     var phase2 = 2 * phasing * time;
 
-    var N = ww - 2;             // XXX hand-hacked
 
     var spacing = diameter * (1 + .2 * Math.sin(breathing * time));
 
@@ -42,6 +68,7 @@ function spinspinspin(time) {
             // (x,y) from http://www.redblobgames.com/grids/hexagons/#hex-to-pixel
             var cx = x0 + (r + g/2) * Math.sqrt(3)/2 * spacing;
             var cy = y0 + g * .75 * spacing;
+            // TODO: morph between these color schemes
             if (false) {
                 drawPieSlice(cx, cy, r/N, time, 'rgba(255,255,255,1)', 0);
                 drawPieSlice(cx, cy, g/N, time, 'rgba(255,255,255,1)', tau/3);
