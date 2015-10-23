018426166fdb80993f7f1c609b6e9b503934e700
Darius Bacon <darius@wry.me>
2015-10-22 22:30:40 -0400

    prettier/clearer display of complexarithmetic

--- complexarithmetic.js
+++ complexarithmetic.js
@@ -5,7 +5,7 @@ var width        = canvas.width;
 var height       = canvas.height;
 var ctx          = canvas.getContext('2d');
 
-ctx.font = '12pt Georgia'
+var font = '12pt Georgia';
 
 var scale = height / 5;
 
@@ -14,51 +14,94 @@ var tau = 2*Math.PI;
 var varA = {re: 0.5, im: 0.5};
 
 function clear() {
+    var i, j;
+    ctx.font = font;
+
     ctx.clearRect(0, 0, width, height);
 
-    ctx.fillStyle = 'white';
-    ctx.fillRect(0, height/2, width, 1);
-    ctx.fillRect(width/2, 0, 1, height);
+    ctx.strokeStyle = 'grey';
+    for (i = 1; (i-1) * scale <= width/2; ++i) { // XXX hack
+        ctx.lineWidth = .5;
+        for (j = 1; j <= 9; ++j) {
+            gridLine(ctx, (i-1 + j/10) * scale, -height/2, (i-1 + j/10) * scale, height/2);
+        }
+        ctx.lineWidth = 1.5;
+        gridLine(ctx,  i * scale, -height/2,  i * scale, height/2);
+    }
+    for (i = 1; (i-1) * scale <= height/2; ++i) { // XXX hack
+        ctx.lineWidth = .5;
+        for (j = 1; j <= 9; ++j) {
+            gridLine(ctx, -width/2, (i-1 + j/10) * scale, width/2, (i-1 + j/10) * scale);
+        }
+        ctx.lineWidth = 1.5;
+        gridLine(ctx, -width/2, i * scale, width/2, i * scale);
+    }
 
+    ctx.fillStyle = 'white';
+    ctx.fillRect(0, height/2-1, width, 3);
+    ctx.fillRect(width/2-1, 0, 3, height);
+
+    ctx.lineWidth = 3;
     ctx.strokeStyle = 'white';
     ctx.beginPath();
     ctx.arc(width/2, height/2, scale, 0, tau, true);
     ctx.closePath();
     ctx.stroke();
 
+    ctx.lineWidth = 1;
     ctx.fillStyle = 'blue';
-    plot({re: 0, im: 0}, '0');
-    plot({re: 1, im: 0}, '1');
-    plot({re: 0, im: 1}, 'i');
+    plot({re: 0, im: 0}, '0', 8, 18);
+    plot({re: 1, im: 0}, '1', 8, 18);
+    ctx.font = 'italic ' + font;
+    plot({re: 0, im: 1}, 'i', 8, 18);
+    ctx.font = font;
 }
 
-function plot(z, label) {
+function gridLine(ctx, x0, y0, x1, y1) {
+    line(ctx, x0, y0, x1, y1);
+    line(ctx, -x0, -y0, -x1, -y1);
+}
+
+function line(ctx, x0, y0, x1, y1) {
+    ctx.beginPath();
+    ctx.moveTo(width/2 + x0 - .5, height/2 - y0 - .5); // - .5 for sharp grid lines
+    ctx.lineTo(width/2 + x1 - .5, height/2 - y1 - .5);
+    ctx.stroke();
+}
+
+function plot(z, label, xOffset, yOffset) {
     var x = z.re * scale + width/2;
     var y = height/2 - z.im * scale;
     ctx.beginPath();
     ctx.arc(x, y, 3, 0, tau);
     ctx.fill();
-    if (label)
-        ctx.fillText(label, x + 7, y + 5);
+    if (label) {
+        if (xOffset === undefined) xOffset = 7;
+        if (yOffset === undefined) yOffset = 5;
+        ctx.fillText(label, x + xOffset, y + yOffset);
+    }
 }
 
 function pointingAt(event) {
     var x = event.clientX - canvasBounds.left;
     var y = event.clientY - canvasBounds.top;
+    x -= 5, y -= 5;             // offset from pointer, to make the point more visible
     return {re: (x - width/2) / scale,
             im: (height/2 - y) / scale};
 }
 
 function show(varB) {
     clear();
-    ctx.fillStyle = 'red';   plot(varA, 'a');
-    ctx.fillStyle = 'green'; plot(varB, 'b');
-    ctx.fillStyle = 'black'; plot(add(varA, varB), 'a+b');
-                             plot(mul(varA, varB), 'ab');
-                             plot(sub(varA, varB), 'a-b');
-                             plot(div(varA, varB), 'a/b');
-                             plot(sub(varB, varA), 'b-a');
-                             plot(div(varB, varA), 'b/a');
+    ctx.fillStyle = 'red';   plot(varA, 'a', -16, -8);
+    ctx.fillStyle = 'green'; plot(varB, 'b', -16, -8);
+
+    ctx.fillStyle = 'black';
+    if (add_checkbox.checked) plot(add(varA, varB), 'a+b');
+    if (mul_checkbox.checked) plot(mul(varA, varB), 'ab');
+    if (sub_checkbox.checked) plot(sub(varA, varB), 'a\u2212b');
+    if (div_checkbox.checked) plot(div(varA, varB), 'a/b');
+    if (subrev_checkbox.checked) plot(sub(varB, varA), 'b\u2212a');
+    if (divrev_checkbox.checked) plot(div(varB, varA), 'b/a');
 }
 
 function onMousemove(event) {
