ca5d01e5618345ac7f932663fe2ae98d0964dc3b
Darius Bacon <darius@wry.me>
2018-03-12 16:57:53 -0700

    add show()

--- connect_n.js
+++ connect_n.js
@@ -4,7 +4,7 @@ const nrows = 6;
 const ncols = 7;
 
 // A board is represented as an array of stacks of pieces. A piece is
-// an integer 0 or 1 denoting which player's.
+// an integer 1 or 2 denoting which player's.
 function makeBoard() {
     const cols = [];
     for (let i = 0; i < ncols; ++i) cols.push([]);
@@ -16,7 +16,23 @@ function drop(board, col, piece) {
     board[col].append(piece);
 }
 
+// The piece at (r,c): 1, 2, or 0 (meaning empty).
+function at(board, r, c) {
+    if (r < 0 || nrows <= r) return 0;
+    const col = board[r];
+    if (c < 0 || cols.length <= c) return 0;
+    return col[c];
+}
+
+const pieces = [' ', 'X', 'O'];
+
 // Return an ASCII representation of the board.
 function show(board) {
-
+    const rows = [];
+    for (let r = 0; r < nrows; ++r) {
+        const row = [];
+        for (let c = 0, c < ncols; ++c) row.push(pieces[at(board, r, c)]);
+        rows.append(row.join(' '));
+    }
+    return rows.join('\n');
 }
